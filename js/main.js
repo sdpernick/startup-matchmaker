f8def8135b176dfe557dd41533e7977c53fae123
sdpernick <sarah.pernick@gmail.com>
2016-11-02 12:32:42 -0400

    Responsive Design

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 document.getElementById("create3").onclick = function(){
 	window.alert("We're not taking sign ups yet.");
 }
+
+$(".hamburger").on("click", function(){
+	
+	if ($(this).hasClass("open")){
+      $(this).removeClass("open");
+      $("header").animate({
+      	height: 70
+      });
+
+   } else {
+   	$(this).addClass("open");
+   	$("header").animate({
+   		height: 300
+   	});
+   }
+   
+});
